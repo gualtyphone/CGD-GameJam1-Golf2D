9dd0b6171acb4cc1a21ffd0509573f3d6f165613
Daniel Donaldson <daniel2.donaldson@live.uwe.ac.uk>
2017-09-30 16:14:12 +0100

    power bar init

---
+++ Assets/Scripts/Power Bar.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBar : MonoBehaviour {
+
+    private float totalWidth = 256;
+    private float powerLevel;
+
+}
