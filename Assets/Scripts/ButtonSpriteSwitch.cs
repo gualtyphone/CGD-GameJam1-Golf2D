cd6dd343b37eca3eb93b8315fe6b7bd43bca2e2d
Daniel Donaldson <daniel2.donaldson@live.uwe.ac.uk>
2017-09-30 15:21:26 +0100

    added main - options - player select menus

---
+++ Assets/Scripts/ButtonSpriteSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System;
+
+public class ButtonSpriteSwitch : MonoBehaviour {
+
+    public Button button;
+    public Sprite buttonImageOn;
+    public Sprite buttonImageOff;
+    public AudioSource audio;
+
+    public void OnOffButton()
+    {
+        if (button.GetComponent<Image>().sprite == buttonImageOn)
+        {
+            button.GetComponent<Image>().sprite = buttonImageOff;
+        }
+        else
+        {
+            button.GetComponent<Image>().sprite = buttonImageOn;
+        }
+    }
+
+    public void muteSound()
+    {
+        audio.mute = !audio.mute;
+    }
+}
