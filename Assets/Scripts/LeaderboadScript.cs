0ed789b46690b2ff09eb77aba853fef490b1210b
Batdan94 <Daniel2.Donaldson@live.uwe.ac.uk>
2017-10-04 00:11:31 +0100

    leaderboard and updated main menu for 1 player

--- Assets/Scripts/LeaderboadScript.cs
+++ Assets/Scripts/LeaderboadScript.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeaderboadScript : MonoBehaviour {
 
-	private Vector2 position1;
-	private Vector2 position2;
-	private Vector2 position3;
-	private Vector2 position4;
+	public GameObject position1;
+	public GameObject position2;
+	public GameObject position3;
+	public GameObject position4;
 
 	private int player1Score = 0;
 	private int player2Score = 0;
@@ -19,12 +20,16 @@ public class LeaderboadScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ps = GameObject.FindObjectOfType<PlayerScript> ();
-		position1.y = -249.7f;
-		position2.y = -296.1f;
-		position3.y = -343.5f;
-		position4.y = -390.9f;
+	}
 
-		if (ps.numOfPlayers == 2) 
+	// Update is called once per frame
+	void Update () 
+	{
+		if (ps.numOfPlayers == 1)
+		{
+			player1Score = ps.getLeaderboard() [0];
+		}
+		else if (ps.numOfPlayers == 2) 
 		{
 			player1Score = ps.getLeaderboard() [0];
 			player2Score = ps.getLeaderboard ()[1];
@@ -41,43 +46,44 @@ public class LeaderboadScript : MonoBehaviour {
 			player3Score = ps.getLeaderboard() [2];
 			player4Score = ps.getLeaderboard() [3];
 		}
-
 		createLeaderboard ();
 	}
 
 	void createLeaderboard ()
-	{		
-		if (ps.numOfPlayers == 2) 
+	{	
+		if ( 1 == 1)
 		{
-			if (player1Score > player2Score)
-			{
-				//ps.p1.transform.position.y = position1.y;
-				//ps.p2.transform.position.y = position2.y;
-			} 
-			else
-			{
-				//ps.p2.transform.position.y = position1.y;
-				//ps.p1.transform.position.y = position2.y;
-			}
+			position1.SetActive (true);
+			position2.SetActive (false);
+			position3.SetActive (false);
+			position4.SetActive (false);
+
+			position1.GetComponentInChildren<Text> ().text = player1Score.ToString();
 		}
-		if (ps.numOfPlayers == 3)
+		else if (ps.numOfPlayers == 2) 
 		{
-			
+			position1.SetActive (true);
+			position2.SetActive (true);
+			position3.SetActive (false);
+			position4.SetActive (false);
+
+		}
+		else if (ps.numOfPlayers == 3)
+		{
+			position1.SetActive (true);
+			position2.SetActive (true);
+			position3.SetActive (true);
+			position4.SetActive (false);
+
 		}
 		if (ps.numOfPlayers == 4) 
 		{
-			
+			position1.SetActive (true);
+			position2.SetActive (true);
+			position3.SetActive (true);
+			position4.SetActive (true);
 		}
-
-
-		ps.p1.transform.position = position1;
+		//ps.p1.transform.position = position1;
 		//ps.p2.transform.position = posi;
-
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
