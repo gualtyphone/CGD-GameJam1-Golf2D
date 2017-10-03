7ce64118b90cda97a76b9efa8a4eac7b85182cf8
ChrisHammie <Christopher2.Hamilton@live.uwe.ac.uk>
2017-10-03 22:56:09 +0100

    Ball doesn't collide while in hole

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -37,6 +37,7 @@ public class Hole : MonoBehaviour {
                 other.GetComponent<Rigidbody2D>().AddForce(((transform.position - other.transform.position) * (Time.deltaTime * speed)));
                 if ((transform.position - other.transform.position).sqrMagnitude < (other.bounds.extents - GetComponent<Collider2D>().bounds.extents).sqrMagnitude)
                 {
+                    other.GetComponent<Collider2D>().isTrigger = true;
                     other.GetComponentInChildren<powerBar>().state = powerBar.ballState.none;
                     other.transform.position = transform.position;
                     other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
