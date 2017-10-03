5ccd04ae1c425e792b48420b5beeaa59b8d7ce76
Gualtiero <gualtyphone@me.com>
2017-10-03 14:04:03 +0100

    Working start, end and walls

--- Assets/Scripts/TextureMap.cs
+++ Assets/Scripts/TextureMap.cs
@@ -30,55 +30,80 @@ public class TextureMap : MonoBehaviour {
 
 	[SerializeField]
 	public GameObject hole;
+
+	[SerializeField]
+	public GameObject wall;
+
+	public List<GameObject> walls;
+
+	public Vector2 startPos = Vector2.zero;
     //[SerializeField]
     //Terrains terrainMouse;
 
     // Use this for initialization
     void Start()
     {
+		walls = new List<GameObject>();
         texMap = GetComponent<SpriteRenderer>().sprite.texture;
         Color[] col = texMap.GetPixels();
-        float dist = 1000000;
-		Vector2 startPos = Vector2.zero;
+		float dist1 = 100000000;
+		float dist2 = 100000000;
 		Vector2 holePos = Vector2.zero;
-        foreach (var terrColor in terrainColors)
-        {
-            if (terrColor.terr == Terrains.START)
-            {
-                for (int x = 0; x < texMap.width; x++)
-                {
-                    for (int y = 0; y < texMap.height; y++)
-                    {
+		for (int x = 0; x < texMap.width; x++)
+		{
+			for (int y = 0; y < texMap.height; y++)
+			{
+		        foreach (var terrColor in terrainColors)
+		        {
+		            if (terrColor.terr == Terrains.START)
+		            {
+						
                         var currentDist = Vector4.Distance(col[x+y*texMap.width], terrColor.col);
-                        if (currentDist < dist)
+                        if (currentDist < dist1)
                         {
-                            dist = currentDist;
+                            dist1 = currentDist;
                             startPos = new Vector2(x, y);
                         }
                     }
-                }
-            }
-            if (terrColor.terr == Terrains.FINISH)
-            {
-                for (int x = 0; x < texMap.width; x++)
-                {
-                    for (int y = 0; y < texMap.height; y++)
-                    {
-                        var currentDist = Vector4.Distance(col[x + y * texMap.width], terrColor.col);
-                        if (currentDist < dist)
-                        {
-                            dist = currentDist;
-                            holePos = new Vector2(x, y);
-                        }
-                    }
-                }
-            }
+					if (terrColor.terr == Terrains.FINISH)
+					{
+						var currentDist = Vector4.Distance(col[x+ (y*texMap.width)], terrColor.col);
+						if (currentDist < dist2)
+						{
+							dist2 = currentDist;
+							holePos = new Vector2(x, y);
+						}
+					}
+					if (terrColor.terr == Terrains.WALL)
+					{
+						if (Vector4.Distance(col[x+ (y*texMap.width)], terrColor.col) <= 0.1f)
+						{
+							walls.Add(Instantiate(wall,new Vector3(x/100.0f +0.005f, y/100.0f+0.005f, 0),new Quaternion()));
+						}
+					}
+				}
+			}
+            
         }
 
         GameObject holeInstance = Instantiate(hole);
-		Vector3 pos = Camera.main.ScreenToWorldPoint(holePos);
-		pos.z = 0.0f;
-		holeInstance.transform.position  = pos;
+		Vector2 position = new Vector2();
+		position.x = holePos.x /100.0f;
+		position.y = holePos.y /100.0f;
+
+		startPos = startPos/100.0f;
+
+		GameObject[] balls = GameObject.FindGameObjectsWithTag("Ball");
+		foreach (GameObject ball in balls)
+		{
+			ball.transform.position = startPos;
+		}
+
+		holeInstance.transform.position = position;
+		//pos.x = (holePos.x / texMap.width * Camera.main.pixelWidth);
+		//pos.y = (holePos.y / texMap.height * Camera.main.pixelHeight);
+		//pos.z = 0.0f;
+		//holeInstance.transform.position = pos;
 
     }
 
