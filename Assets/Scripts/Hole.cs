ab6225872064df76f507f4c4022703d28797b5d2
ChrisHammie <Christopher2.Hamilton@live.uwe.ac.uk>
2017-10-03 22:54:18 +0100

    Player cant't hit ball while in hole

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -11,6 +11,8 @@ public class Hole : MonoBehaviour {
     [SerializeField]
     AudioSource source;
 
+    
+
     // Use this for initialization
     void Start () {
         source = GetComponent<AudioSource>();
@@ -23,19 +25,19 @@ public class Hole : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-
+        
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Ball")
         {
-            if (other.GetComponent<BallController>().enabled == true)
+            if (other.GetComponentInChildren<powerBar>().state != powerBar.ballState.none)
             { 
                 other.GetComponent<Rigidbody2D>().AddForce(((transform.position - other.transform.position) * (Time.deltaTime * speed)));
                 if ((transform.position - other.transform.position).sqrMagnitude < (other.bounds.extents - GetComponent<Collider2D>().bounds.extents).sqrMagnitude)
                 {
-                    other.GetComponent<BallController>().enabled = false;
+                    other.GetComponentInChildren<powerBar>().state = powerBar.ballState.none;
                     other.transform.position = transform.position;
                     other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                     source.PlayOneShot(ballPot, 1f);
